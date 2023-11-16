c95077b23a07f8785816a86b69005157323a8145
i273tama <i520273u@malis.cc.ehime-u.ac.jp>
2023-11-16 10:14:38 +0900

    session.tsの修正

--- src/model/session.ts
+++ src/model/session.ts
@@ -1,11 +1,29 @@
-import {ID} from './id'
-import {Account} from './account'
+import { ID } from './id'
+import { Account } from './account'
+import { nanoid } from 'nanoid'
 
 export class Session {
-    readonly id: ID<Session>;
-    readonly account: Account;
-    readonly loginAt: Date;
-    readonly deviceName: string;
+    constructor(
+        public readonly id: ID<Session>,
+        public readonly account: Account,
+        public readonly loginAt: Date,
+        public readonly deviceName: string,
+    ) {}
+
+    /*現在時刻と認証情報から新しいセッションを作成する*/
+    static async from(clock: Clock, auth: AuthorizationRepository): Promise<Session> {
+        const session = await nanoid();
+        const id = auth.id;
+        const account = auth.account;
+        const loginAt  = await clock.now();
+        const deviceName  = await auth.deviceName();
+        return {
+            id,
+            account,
+            loginAt,
+            deviceName,
+        };
+    }
 }
 
 export type Clock = {
