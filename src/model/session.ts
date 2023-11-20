cea88df73efe5019487525ead8cbe24d6cf96853
MikuroXina <ryosukadnak@gmail.com>
2023-11-20 13:02:29 +0900

    Format

--- src/model/session.ts
+++ src/model/session.ts
@@ -1,27 +1,31 @@
-import { ID } from './id'
-import { Account } from './account'
-import { nanoid } from 'nanoid'
+import { ID } from "./id";
+import { Account } from "./account";
+import { nanoid } from "nanoid";
 
 export class Session {
-    constructor(
-        public readonly id: ID<Session>,
-        public readonly account: Account,
-        public readonly loginAt: Date,
-        public readonly deviceName: string,
-    ) {}
+  constructor(
+    public readonly id: ID<Session>,
+    public readonly account: Account,
+    public readonly loginAt: Date,
+    public readonly deviceName: string,
+  ) {}
 
-    static newSession(clock: Clock, account: Account, deviceName: string): Session{
-        const id = nanoid() as ID <Session>;
-        const loginAt  = clock.now();
-        return {
-            id,
-            account,
-            loginAt,
-            deviceName,
-        };
-    }
+  static newSession(
+    clock: Clock,
+    account: Account,
+    deviceName: string,
+  ): Session {
+    const id = nanoid() as ID<Session>;
+    const loginAt = clock.now();
+    return {
+      id,
+      account,
+      loginAt,
+      deviceName,
+    };
+  }
 }
 
 export type Clock = {
-    now(): Date;
+  now(): Date;
 };
