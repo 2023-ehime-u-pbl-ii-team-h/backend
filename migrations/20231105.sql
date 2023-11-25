1a6861b1d4ebb2b76b70b390d26c65b3fb0e8bb8
i475yama <i520475z@mails.cc.ehime-u.ac.jp>
2023-11-25 20:05:42 +0900

    gitにあがっていた点を修正しました

--- migrations/20231105.sql
+++ migrations/20231105.sql
@@ -1,29 +1,31 @@
-CREATE TABLE IF NOT EXISTS account(
+PRAGMA foreign_keys = ON;
+
+REATE TABLE IF NOT EXISTS 'account'(
 id TEXT PRIMARY KEY,
 name TEXT NOT NULL,
 email TEXT NOT NULL,
 role TEXT NOT NULL CHECK (role IN ('STUDENT', 'TEACHER'))
 );
 
-CREATE TABLE IF NOT EXISTS session( 
+CREATE TABLE IF NOT EXISTS `session`(
 id TEXT NOT NULL PRIMARY KEY,
-account_id NOT NULL,
-login_at NOT NULL,
+account_id TEXT NOT NULL,
+login_at INTEGER NOT NULL,
 device_name TEXT,
 
 FOREIGN KEY (account_id)
 REFERENCES account(id)
 );
 
-CREATE TABLE IF NOT EXISTS subject(
+CREATE TABLE IF NOT EXISTS 'subject'(
 id TEXT NOT NULL PRIMARY KEY,
 name TEXT NOT NULL,       
 );
 
-CREATE TABLE IF NOT EXISTS charge(
+CREATE TABLE IF NOT EXISTS 'charge'(
 id TEXT PRIMARY KEY,
-teacher_id TEXT,
-subject_id TEXT, 
+teacher_id TEXT NOT NULL,
+subject_id TEXT NOT NULL, 
 
 FOREIGN KEY (teacher_id)
 REFERENCES subject(id)
@@ -31,7 +33,7 @@ FOREIGN KEY (subject_id)
 REFERENCES subject(id)
 );
 
-CREATE TABLE IF NOT EXISTS registration(
+CREATE TABLE IF NOT EXISTS 'registration'(
 id TEXT PRIMARY KEY, 
 student_id TEXT, 
 subject_id TEXT,
@@ -42,9 +44,9 @@ FOREIGN KEY (subject_id)
 REFERENCES subject(id)
 );
 
-CREATE TABLE IF NOT EXISTS attendance_board(
+CREATE TABLE IF NOT EXISTS 'attendance_board'(
 id TEXT PRIMARY KEY,
-subject_id subject(id),
+subject_id TEXT NOT NULL,
 start_from INTEGER NOT NULL,
 seconds_from_start_to_be_late INTEGER NOT NULL CHECK (seconds_from_start_to_be_late > 0),
 seconds_from_be_late_to_end INTEGER NOT NULL CHECK (seconds_from_be_late_to_end > 0),
@@ -53,14 +55,14 @@ FOREIGN KEY (subject_id)
 REFERENCES subject(id)
 );
 
-CREATE TABLE IF NOT EXISTS attendance(
+CREATE TABLE IF NOT EXISTS 'attendance'(
 id TEXT PRIMARY KEY,
 created_at INTEGER NOT NULL,
 who TEXT NOT NULL,
-where TEXT NOT NULL,
+'where' TEXT NOT NULL,
 
 FOREIGN KEY (who)
 REFERENCES student(id)
-FOREIGN KEY (where)
+FOREIGN KEY ('where')
 REFERENCES attendance_board(id)
 );
