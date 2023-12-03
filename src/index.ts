66347ab1fcccfb9b3bbc67968492beda59c3d83e
i273tama <i520273u@malis.cc.ehime-u.ac.jp>
2023-12-03 22:52:30 +0900

    index.tsに関数getOrNewAccountを追加

--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,7 @@ const app = new Hono<{
   }
 }>();
 
-const store = new CookieStore()
+const store = new CookieStore();
 
 app.use("*", (c, next) => {
   const middleware = sessionMiddleware({
@@ -58,44 +58,46 @@ app.post("/login", async(c) => {
   const parser = new UAParser(c.req.header("user-agent"));
   const parserResults = parser.getResult(); //デバイス名の取得に必要
   
-  /*DB内にMcrofoft Graphから得られたemailを持つアカウントがあるか探す*/
-  const entry = await c.env.DB.prepare("SELECT * FROM account WHERE email = ?").bind(mails).first();
-  const isNewUser = entry == null;
-  if ( isNewUser ){
-    const account: Account = {
-      id: nanoid() as ID <Account>,
-      email: mails
-    }
-    const statement = c.env.DB.prepare("INSERT INTO account (id, name, email, role) VALUES (?1, ?2, ?3, ?4");
-    if ( role == 'STUDENT' ){
-      const newStudent: Student = {
-        Account: account,
-        role: "STUDENT",
-        enrolling: ID<Subject>[]
+  const account =  await function getOrNewAccount(c.env.DB: Bindings){
+    const entry = await c.env.DB.prepare("SELECT * FROM account WHERE email = ?").bind(mails).first();
+    const isNewUser = entry == null;
+    if ( isNewUser ){
+      const account: Account = {
+        id: nanoid() as ID <Account>,
+        email: mails
+      };
+      const statement = c.env.DB.prepare("INSERT INTO account (id, name, email, role) VALUES (?1, ?2, ?3, ?4");
+      if ( role == 'STUDENT' ){
+        const newStudent: Student = {
+          Account: account,
+          role: "STUDENT",
+          enrolling: ID<Subject>[]
+        };
+        statement.bind(newStudent.id, name, newStudent.email, newStudent.role);
       }
-      statement.bind(newStudent.id, name, newStudent.email, newStudent.role);
-    }
-    else if ( role == 'TEACHER' ){
-      const newTeacher: Teacher = {
-        Account: account,
-        role: "TEACHER",
-        enrolling: ID<Subject>[]
+      else if ( role == 'TEACHER' ){
+        const newTeacher: Teacher = {
+          Account: account,
+          role: "TEACHER",
+          enrolling: ID<Subject>[]
+        };
+        statement.bind(newTeacher.id, name, newTeacher.email, newTeacher.role);
       }
-      statement.bind(newTeacher.id, name, newTeacher.email, newTeacher.role);
+      await statement.run();
     }
-    statement.run();
-  }
-  else {
-    const account: Account = {
-      id: entry['id'] as ID <Account>, 
-      email: entry['email'] as string
+    else {
+      const account: Account = {
+        id: entry['id'] as ID <Account>, 
+        email: entry['email'] as string
+      };
     }
-  }
+    return account;
+  };
   const clock: Clock = {
     now: () => Date.now(),
   };
   const newSession = Session.newSession(clock, account, parserResults.getDevice().type + parserResults.getBrouser().name);
-  c.get('session').set('login', newSession)
+  c.get('session').set('login', newSession);
   const option = {
     status: 200,
   }
