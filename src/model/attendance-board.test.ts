723ca3980d34595ef3b70042b55efec6f2d48a35
i475yama <i520475z@mails.cc.ehime-u.ac.jp>
2023-12-08 22:57:24 +0900

    条件文の追加（以下の6パターンを順に作成） ・開始時刻の 1 秒前 ・開始時刻と同じ時刻 ・遅刻となる時刻の 1 秒前 ・遅刻となる時刻と同じ時刻 ・欠席となる時刻の 1 秒前 ・欠席となる時刻と同じ時刻

--- src/model/attendance-board.test.ts
+++ src/model/attendance-board.test.ts
@@ -18,17 +18,20 @@ type AttendanceState =
 function determineState(board: AttendanceBoard, toAttendAt: Date): AttendanceState {
        
 const currentDateTime = new Date(); // 現在の日時を取得
+const Attendtime = new Date(toAttendAt.getTime() + board.secondsFromStartToBeLate * 1000);//出席と認められる時間
+const Latetime = new Date(toAttendAt.getTime() + board.secondsFromStartToBeLate * 1000 + board.secondsFromBeLateToEnd * 1000);//遅刻と認められる時間
+
 if (currentDateTime < toAttendAt) {
     return "TOO_EARLY";
 } else if(currentDateTime == toAttendAt){
     return "ATTENDED";
-}else if(toAttendAt < currentDateTime && currentDateTime < toAttendAt){
+}else if(toAttendAt < currentDateTime && currentDateTime < Attendtime){
     return "ATTENDED";
-}else if(4){
+}else if(currentDateTime == Attendtime){
     return "BE_LATE";
-}else if(5){
+}else if(Attendtime < currentDateTime && currentDateTime < Latetime){
     return "BE_LATE";
-}else if(6){
+}else if(Latetime <= currentDateTime){
     return "CLOSED";
 }
 
