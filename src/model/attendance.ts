2618d709ec5f8652764001aea55048684a0c5561
i273tama <i520273u@malis.cc.ehime-u.ac.jp>
2023-12-11 11:28:39 +0900

    attendance.tsの追加

---
+++ src/model/attendance.ts
@@ -0,0 +1,10 @@
+import { ID } from "./id";
+import { Student } from "./account";
+import { AttendanceBoard } from "./attendance-board.ts"
+
+export type Attendance = {
+    readonly id: ID<Attendance>;
+    readonly created_at: number;
+    readonly who: ID<Student>;
+    readonly where: ID<AttendanceBoard>;
+};
