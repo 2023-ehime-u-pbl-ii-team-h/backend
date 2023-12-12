907b2c327faea3ee13b3282548482f314d195b78
MikuroXina <ryosukadnak@gmail.com>
2023-12-12 18:15:10 +0900

    Add POST /subjects

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,11 @@ import { HonoSessionRepository } from "./adaptor/session";
 import { MicrosoftGraph } from "./adaptor/microsoft-graph";
 import { MicrosoftOAuth } from "./adaptor/microsoft-oauth";
 import { D1AccountRepository } from "./adaptor/account";
+import { Session } from "./model/session";
+import { ID } from "./model/id";
+import { Subject } from "./model/subject";
+import { nanoid } from "nanoid";
+import { Teacher } from "./model/account";
 
 type Bindings = {
   DB: D1Database;
@@ -74,6 +79,66 @@ app.post(REDIRECT_API_PATH, async (c) => {
 app.post("/logout", async (c) => {
   c.get("session").deleteSession();
   return new Response();
+});
+
+app.post("/subjects", async (c) => {
+  const session = c.get("session");
+  const login = session.get("login") as Session;
+  const body: unknown = await c.req.json();
+
+  if (!(typeof body === "object" && "name" in body && "assignees" in body)) {
+    return c.text("Bad Request", 400);
+  }
+  if (typeof body.name !== "string" || body.name === "") {
+    return c.text("Bad Request", 400);
+  }
+  if (
+    !(
+      Array.isArray(body.assignees) &&
+      body.assignees.length >= 1 &&
+      body.assignees.every(
+        (assignee: unknown): assignee is ID<Teacher> =>
+          typeof assignee === "string",
+      )
+    )
+  ) {
+    return c.text("Bad Request", 400);
+  }
+
+  const assigneesSet = new Set(body.assignees);
+  if (!assigneesSet.has(login.account.id as ID<Teacher>)) {
+    return c.text("Bad Request", 400);
+  }
+
+  const assignees = [...assigneesSet.values()];
+  const statement = c.env.DB.prepare("SELECT * FROM account WHERE id = ?");
+  const rows = await c.env.DB.batch(assignees.map((id) => statement.bind(id)));
+  if (!rows.every((row) => row.results.length === 1)) {
+    return c.text("Bad Request", 400);
+  }
+
+  const newSubjectID = nanoid() as ID<Subject>;
+  const insertCharge = c.env.DB.prepare(
+    "INSERT INTO charge (id, teacher_id, subject_id) VALUES (?, ?, ?)",
+  );
+  await c.env.DB.batch(
+    [
+      c.env.DB.prepare("INSERT INTO subject (id, name) VALUES (?, ?)").bind(
+        newSubjectID,
+        body.name,
+      ),
+    ].concat(
+      assignees.map((assignee) =>
+        insertCharge.bind(nanoid(), assignee, newSubjectID),
+      ),
+    ),
+  );
+
+  return c.json({
+    id: newSubjectID,
+    name: body.name,
+    assignees,
+  });
 });
 
 export default app;
