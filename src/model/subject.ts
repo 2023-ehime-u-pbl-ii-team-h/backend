541e799310e80e0f5837be251a3b72df24f4e1e2
Mikuroさいな <ryosukadnak@gmail.com>
2023-12-12 14:36:12 +0900

    Subject を追加する (#11)

---
+++ src/model/subject.ts
@@ -0,0 +1,42 @@
+import { ID } from "./id";
+// import { AttendanceBoard } from "./attendance-board";
+import { Student, Teacher } from "./account";
+
+export interface AttendanceBoard {
+  startFrom: Date;
+}
+
+export class Subject {
+  constructor(
+    public readonly id: ID<Subject>,
+    public readonly name: string,
+    public readonly boards: readonly AttendanceBoard[],
+    public readonly enrolled: readonly ID<Student>[],
+    public readonly assigned: readonly ID<Teacher>[],
+  ) {}
+
+  shiftAll(days: number, firstIndex: number, lastIndex?: number): Subject {
+    if (!(0 <= firstIndex && firstIndex < this.boards.length)) {
+      throw new Error("`firstIndex` out of range");
+    }
+    if (!Number.isInteger(days)) {
+      throw new Error("`days` is not an integer");
+    }
+    if (lastIndex === undefined) {
+      lastIndex = this.boards.length - 1;
+    }
+    const newBoards = structuredClone(this.boards);
+    for (let i = firstIndex; i <= lastIndex; ++i) {
+      newBoards[i].startFrom.setUTCDate(
+        newBoards[i].startFrom.getUTCDate() + days,
+      );
+    }
+    return new Subject(
+      this.id,
+      this.name,
+      newBoards,
+      this.enrolled,
+      this.assigned,
+    );
+  }
+}
