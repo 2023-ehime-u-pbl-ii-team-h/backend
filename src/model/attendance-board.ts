e380c1764bd28a6b05f16c7ff22c38c284687993
i475yama <i520475z@mails.cc.ehime-u.ac.jp>
2023-12-16 08:53:39 +0900

    名前の変更

---
+++ src/model/attendance-board.ts
@@ -0,0 +1,36 @@
+import { ID } from "./id";
+import { Subject } from "./subject";
+
+export type AttendanceBoard = {
+    readonly id: ID<AttendanceBoard>;
+    readonly subject: ID<Subject>;
+    readonly startFrom: Date;
+    readonly secondsFromStartToBeLate: number;
+    readonly secondsFromBeLateToEnd: number;
+};
+
+export type AttendanceState = 
+    | "TOO_EARLY"
+    | "ATTENDED"
+    | "BE_LATE"
+    | "CLOSED"
+;
+
+export function determineState(board: AttendanceBoard, toAttendAt: Date): AttendanceState {
+       
+    const currentMs = Date.now(); // 現在の日時を取得
+    const beLateMs = toAttendAt.getTime() + board.secondsFromStartToBeLate * 1000;;//出席扱いの時間
+    const endMs = toAttendAt.getTime() + board.secondsFromStartToBeLate * 1000 + board.secondsFromBeLateToEnd * 1000;//遅刻扱いの時間
+
+if (currentMs < toAttendAt.getTime()) {
+    return "TOO_EARLY";
+} 
+if (currentMs < beLateMs){
+    return "ATTENDED";
+}
+if (currentMs < endMs){
+    return "BE_LATE";
+}
+    return "CLOSED";
+
+}
