f55fcac0b84ccfe9aefaef5cc4add0010525a2ff
MikuroXina <ryosukadnak@gmail.com>
2023-12-21 10:01:30 +0900

    Refactor with sumAttendances

--- src/adaptor/attendance.ts
+++ src/adaptor/attendance.ts
@@ -1,5 +1,14 @@
+import { Student } from "../model/account";
 import { Attendance } from "../model/attendance";
+import { ID } from "../model/id";
+import { Subject } from "../model/subject";
 import { AttendanceRepository } from "../service/attend";
+
+export interface AttendancesSum {
+  onTime: number;
+  late: number;
+  miss: number;
+}
 
 export class D1AttendanceRepository implements AttendanceRepository {
   constructor(private readonly db: D1Database) {}
@@ -23,5 +32,43 @@ export class D1AttendanceRepository implements AttendanceRepository {
         )}\n${JSON.stringify(result.error)}`,
       );
     }
+  }
+
+  async sumAttendances(
+    student: ID<Student>,
+    subject: ID<Subject>,
+  ): Promise<AttendancesSum> {
+    const entry = await this.db
+      .prepare(
+        `
+    SELECT
+      SUM(CASE WHEN attendance_board.start_from <= attendance.created_at AND attendance.created_at < (attendance_board.start_from + attendance_board.seconds_from_start_to_be_late) THEN 1 ELSE 0 END) AS on_time,
+      SUM(CASE WHEN (attendance_board.start_from + attendance_board.seconds_from_start_to_be_late) <= attendance.created_at AND attendance.created_at < (attendance_board.start_from + attendance_board.seconds_from_start_to_be_late + attendance_board.seconds_from_be_late_to_end) THEN 1 ELSE 0 END) AS late,
+      SUM(CASE WHEN (attendance_board.start_from + attendance_board.seconds_from_start_to_be_late + attendance_board.seconds_from_be_late_to_end) <= attendance.created_at THEN 1 ELSE 0 END) AS miss
+    FROM attendance
+    INNER JOIN attendance_board
+      ON attendance."where" = attendance_board.id
+    WHERE attendance.who = ?1 AND attendance_board.subject_id = ?2`.replace(
+          /\n/g,
+          "",
+        ),
+      )
+      .bind(student, subject)
+      .first();
+
+    if (entry === null) {
+      throw new Error("SUM up query was invalid");
+    }
+    let { on_time: onTime, late, miss } = entry;
+    if (typeof onTime !== "number") {
+      onTime = 0;
+    }
+    if (typeof late !== "number") {
+      late = 0;
+    }
+    if (typeof miss !== "number") {
+      miss = 0;
+    }
+    return { onTime, late, miss } as AttendancesSum;
   }
 }
