d33155881f7cb7aee5148fcbaa474c3c312a38b0
MikuroXina <ryosukadnak@gmail.com>
2024-01-11 18:46:41 +0900

    Improve Account responsibility

--- src/model/account.ts
+++ src/model/account.ts
@@ -5,14 +5,13 @@ export type Account = {
   readonly id: ID<Account>;
   readonly name: string;
   readonly email: string;
+  readonly role: string;
 };
 
 export type Student = Account & {
   readonly role: "STUDENT";
-  readonly enrolling: readonly ID<Subject>[];
 };
 
 export type Teacher = Account & {
   readonly role: "TEACHER";
-  readonly assigned: readonly ID<Subject>[];
 };
