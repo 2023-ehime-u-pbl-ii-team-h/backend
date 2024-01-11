3c75dcf36fad849f2d2a19450d39de58638c0dcc
MikuroXina <ryosukadnak@gmail.com>
2024-01-12 00:20:59 +0900

    Implement studentsByEachSubjects

--- src/adaptor/subject-student.ts
+++ src/adaptor/subject-student.ts
@@ -4,11 +4,26 @@ import { Subject } from "../model/subject";
 export class D1SubjectStudentRepository {
   constructor(private readonly db: D1Database) {}
 
-  studentsByEachSubjects(teachers: readonly Subject[]): Promise<Student[][]> {
-    throw new Error("todo");
+  async studentsByEachSubjects(
+    subjects: readonly Subject[],
+  ): Promise<Student[][]> {
+    const selectStudent = this.db.prepare(
+      "SELECT account.id, account.name, account.email, account.role FROM registration INNER JOIN account ON registration.student_id = account.id AND registration.subject_id = ?1 AND account.role = 'STUDENT'",
+    );
+
+    const resultsBySubject = await this.db.batch<Student>(
+      subjects.map(({ id }) => selectStudent.bind(id)),
+    );
+
+    return resultsBySubject.map(({ results }) => {
+      if (!results) {
+        throw new Error("related account query failed");
+      }
+      return results;
+    });
   }
 
-  subjectsByEachStudent(teachers: readonly Student[]): Promise<Subject[][]> {
+  subjectsByEachStudent(students: readonly Student[]): Promise<Subject[][]> {
     throw new Error("todo");
   }
 }
